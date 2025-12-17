ef2c78ce0e148aec3ba5be3eae8d3d2056a0fcb1
Bruno Eneas <brunovn.eneas@gmail.com>
2025-12-17 17:05:43 -0300

    feat: grpc project v2

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 GITHUB_USERNAME=ebrunovs
 GITHUB_EMAIL=brunovs.eneas@gmail.com
 
-SERVICE_NAME=order
+SERVICE_NAME=payment
 RELEASE_VERSION=v1.2.3
 
 # Adiciona o protoc ao PATH (instalado via winget)
